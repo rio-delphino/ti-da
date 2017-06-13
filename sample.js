391e2e518f25137007d38d5f3417617c13a23343
rio-delphino <ryo.heavenlysky@gmail.com>
2017-06-14 00:02:37 +0900

    Create sample.js

--- sample.js
+++ sample.js
@@ -1,16 +1,22 @@
-// HTMLがロードされたら
+// jsファイル読込と同時に実行
+alert ("0: 直接記述;");
+
+// DOMがロードされたら
 document.addEventListener('DOMContentLoaded', function() {
     alert ("A: document.addEventListener('DOMContentLoaded', function() { 処理 };");
 });
 
+// 読込完了したら
 $(document).ready( function() {
     alert ("B: $(document).ready( function() { 処理 };");
 });
 
+// もっと後。
 window.onload = function() {
     alert ("C1: window.onload = function() { 処理 };");
 };
 
+// もっと後。
 $(window).on("load", function() {
     alert ("C2: $(window).on('load', function() { 処理 };");
 });
