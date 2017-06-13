52a530eb201b01a8f30d9c9629eace1c6867c611
rio-delphino <ryo.heavenlysky@gmail.com>
2017-06-13 23:31:55 +0900

    Create sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     alert ("外部ファイル sample.js です。\n DOMContentLoaded イベントが発火しました。");
 });
+
+window.onload = function() {
+    alert ("外部ファイル sample.js です。\n すべてのページ読込が完了しました。");
+};
