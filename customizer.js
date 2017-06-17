9f485ddab5f89228bcc0468350a924902c9c4cd4
rio-delphino <ryo.heavenlysky@gmail.com>
2017-06-17 21:49:30 +0900

    Update customizer.js

--- customizer.js
+++ customizer.js
@@ -319,7 +319,7 @@ function reformatSidebar () {
                     // div.sidetitle を削除
                     $target.find(".sidetitle").remove;
                     // 替わりとなる html を生成
-                    $html = "<i class='fa fa-circle-o' aria-hidden='true'></i><span class='" + $className[$i] + "'>" + $jpTitle + "</span><span class='sideTitleSub'>" + $engTitle + "</span></div>";
+                    $html = "<i class='fa fa-circle-o' aria-hidden='true'></i><span class='" + $className[$i] + "'>" + $jpTitle[$i] + "</span><span class='sideTitleSub'>" + $engTitle[$i] + "</span></div>";
                     alert ($html);
                     // 要素内先頭に挿入
                     $target.prepend($html);
